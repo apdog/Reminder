0403bd0b5a1165084951c4398985c8985251de37
apdog <alex.puhow@gmail.com>
2023-07-18 22:14:33 +0300

    добавлена логика NoticeItemFragment

--- app/src/main/java/com/example/reminder/presentation/NoticeItemFragment.kt
+++ app/src/main/java/com/example/reminder/presentation/NoticeItemFragment.kt
@@ -1,13 +1,33 @@
 package com.example.reminder.presentation
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.reminder.R
+import com.example.reminder.domain.NoticeItem
+import com.google.android.material.textfield.TextInputLayout
 
-class NoticeItemFragment : Fragment() {
+class NoticeItemFragment(
+    private var screenMode: String = MODE_UNKNOWN,
+    private var noticeItemId: Int = NoticeItem.UNDEFINED_ID
+) : Fragment() {
+
+    private lateinit var viewModel: NoticeItemViewModel
+
+    private lateinit var tilName: TextInputLayout
+    private lateinit var tilDescription: TextInputLayout
+    private lateinit var etName: EditText
+    private lateinit var etDescription: EditText
+    private lateinit var buttonSave: Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,5 +35,126 @@ class NoticeItemFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_notice_item, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        parseParams()
+        viewModel = ViewModelProvider(this)[NoticeItemViewModel::class.java]
+        initViews(view)
+        addTextChangedListeners()
+        launchRightMode()
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.errorInputDescription.observe(viewLifecycleOwner) {
+            val message = if (it) {
+                getString(R.string.error_input_description)
+            } else {
+                null
+            }
+            tilDescription.error = message
+        }
+        viewModel.errorInputName.observe(viewLifecycleOwner) {
+            val message = if (it) {
+                getString(R.string.error_input_name)
+            } else {
+                null
+            }
+            tilName.error = message
+        }
+        viewModel.shouldCloseScreen.observe(viewLifecycleOwner) {
+            finish()
+        }
+    }
+
+    private fun launchRightMode() {
+        when (screenMode) {
+            MODE_EDIT -> launchEditMode()
+            MODE_ADD -> launchAddMode()
+        }
+    }
+
+    private fun addTextChangedListeners() {
+
+        etName.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.resetErrorInputName()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+
+        etDescription.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.resetErrorInputDescription()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+    }
+
+    private fun launchEditMode() {
+        viewModel.getNoticeItem(noticeItemId)
+        viewModel.noticeItem.observe(viewLifecycleOwner) {
+            etName.setText(it.name)
+            etDescription.setText(it.description)
+        }
+        buttonSave.setOnClickListener() {
+            viewModel.editNoticeItem(etName.text?.toString(), etDescription.text?.toString())
+        }
+    }
+
+    private fun launchAddMode() {
+        buttonSave.setOnClickListener() {
+            viewModel.addNoticeItem(etName.text?.toString(), etDescription.text?.toString())
+        }
+    }
+
+    private fun parseParams() {
+        if (screenMode != MODE_EDIT && screenMode != MODE_ADD) {
+            throw RuntimeException("Param screen mode is absent")
+        }
+        if (screenMode == MODE_EDIT && noticeItemId == NoticeItem.UNDEFINED_ID) {
+            throw RuntimeException("Param notice item id is absent")
+        }
+    }
+
+    private fun initViews(view: View) {
+        tilName = view.findViewById(R.id.til_name)
+        tilDescription = view.findViewById(R.id.til_description)
+        etName = view.findViewById(R.id.et_name)
+        etDescription = view.findViewById(R.id.et_desc)
+        buttonSave = view.findViewById(R.id.save_button)
+    }
+
+    companion object {
+        private const val EXTRA_SCREEN_MODE = "extra_mode"
+        private const val EXTRA_NOTICE_ITEM_ID = "extra_notice_item_id"
+        private const val MODE_EDIT = "mode_edit"
+        private const val MODE_ADD = "mode_add"
+        private const val MODE_UNKNOWN = ""
+
+        fun newIntentAddItem(context: Context): Intent {
+            val intent = Intent(context, NoticeItemActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_ADD)
+            return intent
+        }
+
+        fun newIntentEditItem(context: Context, noticeItemId: Int): Intent {
+            val intent = Intent(context, NoticeItemActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_EDIT)
+            intent.putExtra(EXTRA_NOTICE_ITEM_ID, noticeItemId)
+            return intent
+        }
     }
 }
